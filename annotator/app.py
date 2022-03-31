acba60411eea0444136d11adab013d4451f6a47b
Morgan <morgan.nolan@hotmail.com>
2022-03-31 01:41:48 +0100

    Added endpoints to communicate with previously added JavaScript functions.

--- annotator/app.py
+++ annotator/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, request
 
 
 app = Flask(__name__)
@@ -17,6 +17,33 @@ def styles():
 @app.route("/script.js")
 def scripts():
     return send_from_directory("static/js", "script.js")
+
+
+@app.route("/process-evaluation", methods=["POST"])
+def process_evaluation():
+    data = request.get_json()
+    id = data["id"]
+    annotation = data["annotation"]
+    print("id:", id)
+    print("annotation:", annotation)
+    #
+    # TODO: Process evaluation
+    #
+    return ("", 204)
+
+
+@app.route("/get-random-row")
+def get_random_row():
+    data = {
+        "id": "success-1",
+        "title": "success-2",
+        "ticker_list": "success-3",
+        "subreddit_display_name": "success-4",
+        "author_name": "success-5",
+        "url": '<a href="/#" target="blank_">success-6</a>',
+        "text": "success",
+    }
+    return data
 
 
 if __name__ == "__main__":
