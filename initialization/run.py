5690c7ec4f78d05703ccb0de2bd9c4f59bd42c56
Morgan <morgan.nolan@hotmail.com>
2022-04-22 09:04:17 +0100

    Made changes to run.py to reflect the past dozen or so commits.

--- initialization/run.py
+++ initialization/run.py
@@ -1,8 +1,9 @@
+import create_query as create_query
+import retriever as retriever
+import database
 import sys
 
-import create_query as create_query
-import database
-import retriever
+from datetime import datetime
 
 
 def main():
@@ -11,10 +12,12 @@ def main():
 
     if arg_length < 2:
         print(
-            "--create-json: creates table json files",
-            "--init-insert: executes initial data insertion\n",
-            "--delete-tables: deletes all tables in database\n",
-            "--debug: utilized for development\n",
+            "\nflags:" "\n\t--create-json: creates table json files",
+            "\n\t--init-insert: executes initial data insertion",
+            "\n\t--create-tables: creating tables"
+            "\n\t--delete-tables: deletes all tables in database",
+            "\n\t--patch: apply a patch",
+            "\n\t--debug: utilized for development",
         )
 
     else:
@@ -22,39 +25,52 @@ def main():
             arg = sys.argv[i]
 
             if arg == "--create-json":
-                print("--init-insert: creating json")
+                print("\n--init-insert: creating json")
                 create_query.export_all()
 
             elif arg == "--init-insert":
-                print("--init-insert: carrying out initial insertion")
-                # TODO
+                print("\n--init-insert: carrying out initial insertion")
+                reddit = retriever.Reddit()
+
+                # retrieving posts from reddit, and columns list from db
+                after = int(datetime(2021, 9, 20).timestamp())  # past 6 months
+                for subreddit in retriever.SUBREDDITS:
+                    posts = reddit.posts__from_subreddit(subreddit, after)
+
+                    # inserting posts
+                    for i, post in enumerate(posts):
+                        db.insert_row("unannotated_posts", post)
+
+            elif arg == "--create-tables":
+                print("\n--create-tables: creating tables")
+                for table in database.TABLES:
+                    db.create_table(table)
 
             elif arg == "--drop-tables":
-                print("--delete-tables: deleting tables")
+                print("\n--delete-tables: deleting tables")
                 for table in create_query.TABLES:
                     db.drop_table(table)
 
+            elif arg == "--patch":
+                print("\n--patch: apply a patch")
+                print("No patch implemented")
+
             elif arg == "--debug":
-                print("--debug: debugging implementation")
+                print("\n--debug: debugging implementation")
                 create_query.export_all()
 
                 db.drop_table("unannotated_posts")
                 db.create_table("unannotated_posts")
                 reddit = retriever.Reddit()
-                
+
                 # retrieving posts from reddit, and columns list from db
-                posts = reddit.posts__from_subreddit__since__limited_to("Stocks", "month", 10)
-                columns = db.get_columns_list("unannotated_posts")
+                after = int(datetime(2022, 4, 22).timestamp())  # since start of today
+                posts = reddit.posts__from_subreddit("stocks", after)
 
                 # inserting posts
                 for i, post in enumerate(posts):
-                    print(f"\n{i}.\nTickers: {post[8]}\nLink: https://reddit.com/r/stocks/comments/{post[4]}")
-                    res = db.insert_row("unannotated_posts", post)
-                    if res == False:
-                        print("I crashed")
-                        break
-
-            
+                    print(f"\n{i}.\nTickers: {post[8]}\nLink: {post[18]}")
+                    db.insert_row("unannotated_posts", post)
 
 
 if __name__ == "__main__":
