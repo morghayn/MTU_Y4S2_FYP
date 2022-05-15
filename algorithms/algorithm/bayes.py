17ec1e5269ae1fb2a8901ae55f2b552b4bb2f39c
Morgan Nolan <47060871+morghayn@users.noreply.github.com>
2022-05-16 00:29:36 +0100

    Pushing MultinomialNB implementation.

--- algorithms/algorithm/bayes.py
+++ algorithms/algorithm/bayes.py
@@ -1,41 +1,73 @@
-import pandas as pd
-from sklearn.naive_bayes import MultinomialNB
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from tqdm import tqdm
+
+from utilities.processing import data_preprocessing, split_data
+from utilities.plot import plot_confusion_matrix, print_statistics
 
 
 def run(df):
-    x, x_test, y, y_test = transform(df)
-    model = MultinomialNB()
+   LABEL = "Count Vectors MultinomialNB"
+   # LABEL = "GaussianNB"
+   df = data_preprocessing(df)
+   y_tests = np.empty([0], dtype=int)
+   y_predictions = np.empty([0], dtype=int)
 
-    model.fit(x, y)
-    score = model.score(x_test, y_test)
-    print(score)
+   kfold = KFold(n_splits=10, random_state=42, shuffle=True)
+   for train, test in tqdm(kfold.split(df)):
+      x_train, y_train, x_test, y_test = tokenize(df.iloc[train], df.iloc[test])
+      model = MultinomialNB()
+      model.fit(x_train, y_train)
+      y_tests =  np.append(y_tests, y_test, axis=0)
+      y_predictions = np.append(y_predictions, model.predict(x_test), axis=0)
+    
+   print_statistics(y_tests, y_predictions)
+   plot_confusion_matrix(y_tests, y_predictions, LABEL)
+   plt.show()
 
 
-def transform(df):
-    # creating new dataframe with specific columns
-    df = df[["title", "text", "sentiment"]]
+def tokenize(train_df, test_df):
+   # vectorizing data, i.e., converting text to numbers
+   # vectorizer = CountVectorizer(stop_words="english")
+   vectorizer = CountVectorizer()
+   # vectorizer = TfidfVectorizer(stop_words="english")
+   # vectorizer = TfidfVectorizer()
+   x_train = vectorizer.fit_transform(train_df.text).toarray()
+   x_test = vectorizer.transform(test_df.text).toarray()
+   y_train = train_df.sentiment
+   y_test = test_df.sentiment
+   return x_train, y_train, x_test, y_test
 
-    # appending title as start of text cells and then dropping title column from df
-    df = df.assign(text=(df["title"] + " " + df["text"]))
 
-    # converting all text to lower case
-    df = df.applymap(lambda s: s.lower() if type(s) == str else s)
+"""
+CountVectorize()
+Accuracy-score: 0.7099273607748184
+Class-report:
+              precision    recall  f1-score   support
 
-    # splitting dataset
-    x = df["text"]
-    y = df["sentiment"]
-    x, x_test, y, y_test = train_test_split(
-        x, y, stratify=y, test_size=0.25, random_state=42
-    )
+           0       0.71      0.71      0.71      2065
+           1       0.71      0.71      0.71      2065
 
-    # vectorizing data, i.e., converting text to numbers
-    vectorizer = CountVectorizer(stop_words="english")
-    # vectorizer = TfidfVectorizer(stop_words="english")
-    # vectorizer = CountVectorizer()
-    x = vectorizer.fit_transform(x).toarray()
-    x_test = vectorizer.transform(x_test).toarray()
-    print("x_test", x)
+    accuracy                           0.71      4130
+   macro avg       0.71      0.71      0.71      4130
+weighted avg       0.71      0.71      0.71      4130
 
-    return x, x_test, y, y_test
+F1-score: 0.7097868217054263
+
+TfidfVectorizer()
+Accuracy-score: 0.7033898305084746
+Class-report:
+              precision    recall  f1-score   support
+
+           0       0.79      0.55      0.65      2065
+           1       0.66      0.86      0.74      2065
+
+    accuracy                           0.70      4130
+   macro avg       0.72      0.70      0.70      4130
+weighted avg       0.72      0.70      0.70      4130
+
+F1-score: 0.7429171038824764
+"""
